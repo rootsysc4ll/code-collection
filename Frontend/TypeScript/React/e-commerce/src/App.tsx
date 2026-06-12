e06fd8f12d3e37dd0e2b3a70eac24b64afd52744
i4u9 <dosakik13@gmail.com>
2026-06-12 11:43:22 -0300

    moved orders and checkout page

--- Frontend/TypeScript/React/e-commerce/src/App.tsx
+++ Frontend/TypeScript/React/e-commerce/src/App.tsx
@@ -2,11 +2,15 @@ import { Routes, Route } from 'react-router'
 import './styles/App.css'
 
 import HomePage from './pages/HomePage'
+import CheckoutPage from './pages/CheckoutPage'
+import OrdersPage from './pages/OrdersPage'
 
 function App() {
     return (
         <Routes>
-            <Route path='/' element={<HomePage />} ></Route>
+            <Route path='/'         element={<HomePage     />} />
+            <Route path='/checkout' element={<CheckoutPage />} />
+            <Route path='/orders'   element={<OrdersPage />}   />
         </Routes>
     )
 }
--- Frontend/TypeScript/React/e-commerce/src/pages/HomePage.tsx
+++ Frontend/TypeScript/React/e-commerce/src/pages/HomePage.tsx
@@ -1,11 +1,11 @@
 import "./../styles/HomePage.css"
 
-function HomePage() {
+export default function HomePage() {
     return (
         <>
             <div className="header">
                 <div className="left-section">
-                    <a href="index.html" className="header-link">
+                    <a href="/" className="header-link">
                     <img className="logo"
                         src="images/logo-white.png" />
                     <img className="mobile-logo"
@@ -22,12 +22,12 @@ function HomePage() {
                 </div>
 
                 <div className="right-section">
-                    <a className="orders-link header-link" href="orders.html">
+                    <a className="orders-link header-link" href="/orders">
 
                     <span className="orders-text">Orders</span>
                     </a>
 
-                    <a className="cart-link header-link" href="checkout.html">
+                    <a className="cart-link header-link" href="/checkout">
                     <img className="cart-icon" src="images/icons/cart-icon.png" />
                     <div className="cart-quantity">3</div>
                     <div className="cart-text">Cart</div>
@@ -188,5 +188,3 @@ function HomePage() {
         </>
     )
 }
-
-export default HomePage
