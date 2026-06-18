7e869a04b425140daffed96b923e570ee767b817
i4u9 <dosakik13@gmail.com>
2026-06-17 23:50:14 -0300

    creating test setup for the product component

---
+++ Frontend/TypeScript/React/ecommerce/src/pages/home/Product.tsx
@@ -0,0 +1,73 @@
+import axios from "axios"
+import { useState } from "react"
+
+import checkmark from "../../assets/images/icons/checkmark.png"
+import { formatMoney } from "../../utils/functions"
+import type { ProductType } from "../../utils/types"
+
+type Props = {
+    product: ProductType
+    loadCart: () => void
+}
+
+export default function Product({ product, loadCart }: Props) {
+    const [ quantity, setQuantity ] = useState<number>(1)
+    const [ addded, setAddded ]     = useState<boolean>(false)
+
+    async function handleAddToCart(){
+        await axios.post('/api/cart-items', {
+            productId: product.id,
+            quantity
+        })
+        loadCart()
+        
+        setAddded(true)
+        setTimeout(() => {
+            setAddded(false)
+        }, 1500);
+    }
+
+    return (
+        <div className="product-container">
+            <div className="product-image-container">
+                <img className="product-image"
+                    src={product.image} />
+            </div>
+
+            <div className="product-name limit-text-to-2-lines">{product.name}</div>
+
+            <div className="product-rating-container">
+                <img className="product-rating-stars"
+                    data-testid='product-rating-stars'
+                    src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
+                <div className="product-rating-count link-primary">{product.rating.count}</div>
+            </div>
+
+            <div className="product-price">{formatMoney(product.priceCents)}</div>
+
+            <div className="product-quantity-container">
+                <select value={quantity} onChange={e => setQuantity(Number(e.target.value))} >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                </select>
+            </div>
+
+            <div className="product-spacer"></div>
+
+            <div className="added-to-cart" style={{ opacity: addded ? '1' : '0' }}>
+                <img src={checkmark} />
+                Added
+            </div>
+
+            <button className="add-to-cart-button button-primary" onClick={handleAddToCart}>Add to Cart</button>
+        </div>
+    )
+}
