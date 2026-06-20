772e9fbe9db20a90035cb62697d9de9358f7cde5
i4u9 <dosakik13@gmail.com>
2026-06-20 11:00:33 -0300

    starting backend course

---
+++ Backend/Typescript/backend-introo/dist/server.js
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`opened server on port ${PORT}`);
+});
+//# sourceMappingURL=server.js.map
